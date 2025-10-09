0d956523d228763473b0c53d39afecfd3f88b8ca
akpu-odoo <akpu@odoo.com>
2025-10-09 14:56:03 +0530

    [add] : Add Skeleton for module

---
+++ rb_titoli/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Rb Titoli - Import Koala Data",
+    'version': '19.0.1.0.0',
+    'summary': "Import Data from Koala API's",
+    'description': """
+        Andrea Mottola - Prenotazione || Task Id - 5138121
+        =====================================================
+        Module to Import data from Koala API's.
+    """,
+    'category': 'custom',
+
+    # Author
+    'author': 'Odoo PS',
+    'website': 'https://www.odoo.com',
+    'license': 'LGPL-3',
+
+    # Dependency
+    'depends': [],
+    'data': [],
+    'installable': True,
+}
