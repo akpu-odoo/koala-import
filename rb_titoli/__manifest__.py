d010b8aebe2e0a0a86464f4b74bdb033f1e9b891
akpu-odoo <akpu@odoo.com>
2025-10-09 17:27:35 +0530

    [add] : Add API Service

--- rb_titoli/__manifest__.py
+++ rb_titoli/__manifest__.py
@@ -2,23 +2,23 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': "Rb Titoli - Import Koala Data",
-    'version': '19.0.1.0.0',
-    'summary': "Import Data from Koala API's",
-    'description': """
+    "name": "Rb Titoli - Import Koala Data",
+    "version": "18.0.1.0.0",
+    "summary": "Import Data from Koala API's",
+    "description": """
         Andrea Mottola - Prenotazione || Task Id - 5138121
         =====================================================
         Module to Import data from Koala API's.
     """,
-    'category': 'custom',
-
+    "category": "custom",
     # Author
-    'author': 'Odoo PS',
-    'website': 'https://www.odoo.com',
-    'license': 'LGPL-3',
-
+    "author": "Odoo PS",
+    "website": "https://www.odoo.com",
+    "license": "LGPL-3",
     # Dependency
-    'depends': [],
-    'data': [],
-    'installable': True,
+    "depends": ["base_setup"],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "installable": True,
 }
